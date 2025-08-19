f11de4a6f243ebdd0e0e36fe7782ecc5084d3dce
Samuel Alves <samalves.dev@gmail.com>
2025-08-18 22:26:56 -0300

    Subida do Projeto

---
+++ src/main/java/app/iwaiter/IwaiterApplication.java
@@ -0,0 +1,13 @@
+package app.iwaiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwaiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwaiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/iwaiter/IwaiterApplicationTests.java
@@ -0,0 +1,13 @@
+package app.iwaiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwaiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
