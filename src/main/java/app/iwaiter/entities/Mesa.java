0373321f704a97cd29035b39011569f652595366
Samuel Alves <samalves.dev@gmail.com>
2025-08-19 00:33:45 -0300

    feat: Ajuste na entidade Mesa.

--- src/main/java/app/iwaiter/entities/Mesa.java
+++ src/main/java/app/iwaiter/entities/Mesa.java
@@ -6,8 +6,37 @@ import jakarta.persistence.*;
 public class Mesa {
     @Id
     private Long numero;
-    private String restauranteID;
+    private Long capacidade;
+    private Boolean disponivel;
 
     @ManyToOne
     private Garcom garcomResponsavel;
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    public Long getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(Long capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public Boolean getDisponivel() {
+        return disponivel;
+    }
+
+    public Garcom getGarcomResponsavel() {
+        return garcomResponsavel;
+    }
+
+    public void setGarcomResponsavel(Garcom garcomResponsavel) {
+        this.garcomResponsavel = garcomResponsavel;
+    }
 }
