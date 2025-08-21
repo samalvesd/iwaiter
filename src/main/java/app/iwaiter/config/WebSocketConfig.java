dac58a2e5a8ee279ab4e1a9a4288a1efa83ffcd1
Samuel Alves <samalves.dev@gmail.com>
2025-08-21 00:04:34 -0300

    feat: Criação da configuração do WebSocket.

---
+++ src/main/java/app/iwaiter/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package app.iwaiter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic"); // Prefixo para tópicos que serão enviados para o frontend
+        config.setApplicationDestinationPrefixes("/app"); // Prefixo para endpoints que o frontend envia
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS(); // Endpoint de conexão WebSocket
+    }
+}
